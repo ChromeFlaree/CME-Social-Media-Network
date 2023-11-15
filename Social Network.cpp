d9dba505a2650bd2ae06a374b72d521829d61668
Sourasish Chakraborty <150173837+ChromeFlaree@users.noreply.github.com>
2023-11-16 01:08:51 +0530

    Update Social Network.cpp

--- Social Network.cpp
+++ Social Network.cpp
@@ -7,8 +7,13 @@
 class SocialNetwork {
 public:
     void addUser(int id, string name, int age, string gender) {
-        User* newUser = new User(id, name, age, gender);
-        users[id] = newUser;
+        if (users.find(id) == users.end()) {
+            User* newUser = new User(id, name, age, gender);
+            users[id] = newUser;
+        }
+        else {
+            cout << "Error : User with ID " << id << " already exists. Choose a unique ID." << endl;
+        }
     }
 
     void deleteUser(int id) {
@@ -16,6 +21,15 @@ public:
         if (it != users.end()) {
             delete it->second;
             users.erase(it);
+        }
+    }
+
+    void addFriendship(int userId1, int userId2) {
+        auto user1It = users.find(userId1);
+        auto user2It = users.find(userId2);
+        if (user1It != users.end() && user2It != users.end()) {
+            user1It->second->addFriend(user2It->second);
+            user2It->second->addFriend(user1It->second);
         }
     }
 
@@ -47,6 +61,16 @@ public:
             }
         }
         return result;
+    }
+
+    unordered_set<int> getFriendsOfUser(int userId) const {
+        unordered_set<int> friendIds;
+        auto it = users.find(userId);
+        if (it != users.end()) {
+            const auto& friends = it->second->getFriendIds();
+            friendIds.insert(friends.begin(), friends.end());
+        }
+        return friendIds;
     }
 
 private:
