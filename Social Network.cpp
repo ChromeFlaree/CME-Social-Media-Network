83a431b7bc636fa0c81280f1de8bfefea5e2edb9
Sourasish Chakraborty <150173837+ChromeFlaree@users.noreply.github.com>
2023-11-17 21:43:11 +0530

    Update Social Network.cpp

--- Social Network.cpp
+++ Social Network.cpp
@@ -3,10 +3,12 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <vector>
+#include <memory>
+using namespace std;
 
 class SocialNetwork {
 public:
-    void addUser(User* user) {
+    void addUser(shared_ptr<User> user) {
         if (users.find(user->getId()) == users.end()) {
             users[user->getId()] = user;
         }
@@ -18,33 +20,32 @@ public:
     void deleteUser(int id) {
         auto it = users.find(id);
         if (it != users.end()) {
-            delete it->second;
             users.erase(it);
         }
     }
 
-    vector<User*> searchByName(const string name) {
-        vector<User*> result;
+    vector<shared_ptr<User>> searchByName(const string name) {
+        vector<shared_ptr<User>> result;
         for (const auto userPair : users) {
-            if (userPair.second->name == name) {
+            if (userPair.second->getName() == name) {
                 result.push_back(userPair.second);
             }
         }
         return result;
     }
 
-    vector<User*> searchByAge(int age) {
-        vector<User*> result;
+    vector<shared_ptr<User>> searchByAge(int age) {
+        vector<shared_ptr<User>> result;
         for (const auto userPair : users) {
-            if (userPair.second->age == age) {
+            if (userPair.second->getAge() == age) {
                 result.push_back(userPair.second);
             }
         }
         return result;
     }
 
-    vector<User*> searchByHobbies(const string hobby) {
-        vector<User*> result;
+    vector<shared_ptr<User>> searchByHobbies(const string hobby) {
+        vector<shared_ptr<User>> result;
         for (const auto userPair : users) {
             if (userPair.second->hobbies.find(hobby) != userPair.second->hobbies.end()) {
                 result.push_back(userPair.second);
@@ -64,5 +65,5 @@ public:
     }
 
 private:
-    unordered_map<int, User*> users;
+    unordered_map<int, shared_ptr<User>> users;
 };
