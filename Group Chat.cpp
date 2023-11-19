c92ceb585756904aaf51d93df0c7bc464f7faac6
Sourasish Chakraborty <150173837+ChromeFlaree@users.noreply.github.com>
2023-11-19 18:14:55 +0530

    Update Group Chat.cpp

--- Group Chat.cpp
+++ Group Chat.cpp
@@ -8,22 +8,45 @@ using namespace std;
 
 class GroupChat {
 public:
-    void addUser(const shared_ptr<User> user) { // Adding users to the group to chat
-        members.insert(user);
+    void GroupChat::addUser(const shared_ptr<User> user) {
+        if (!blockedUsers.count(user) && members.insert(user).second) {
+            cout << user->getName() << " joined the group chat." << endl;
+        } 
+        else {
+            cerr << "Error: User " << user->getName() << " cannot join the group chat." << endl;
+        }
     }
 
-    void removeUser(const shared_ptr<User> user) { // Removing users from the group
+    void removeUser(const shared_ptr<User> user) { 
         members.erase(user);
+        blockedUsers.erase(user);
     }
 
-    void sendMessage(const shared_ptr<User> sender, const string message) const { // Function to send messages in the group
+    void GroupChat::blockUser(const shared_ptr<User> blocker, const shared_ptr<User> blockedUser) {
+        if (members.count(blocker) && members.count(blockedUser)) {
+            blockedUsers.insert(blockedUser);
+            blockedUser->removeFriend(blocker);
+            cout << blocker->getName() << " blocked " << blockedUser->getName() << " in the group chat." << endl;
+        } 
+        else {
+            cerr << "Error : Unable to block user. Users must be members of the group." << endl;
+        }
+    }
+
+    void GroupChat::sendMessage(const shared_ptr<User> sender, const string message) const {
         for (const auto member : members) {
-            if (member != sender) {
-                cout << sender->getName() << " in group chat : " << message << endl; // Displaying message along with name of sender
+            if (member != sender && !sender->isBlocked(member) && sender->isFriend(member)) {
+                cout << sender->getName() << " in group chat: " << message << endl;
             }
         }
     }
 
+    bool GroupChat::isUserInGroup(const shared_ptr<User> user) const {
+        return members.count(user) > 0;
+    }
+
 private:
     unordered_set<shared_ptr<User>> members;
+    unordered_set<shared_ptr<User>> blockedUsers;
+    friend class User;
 };
