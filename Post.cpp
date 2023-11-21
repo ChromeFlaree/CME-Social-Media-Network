5de8a53308b8789ba221124da561549c9f2f8e5b
Sourasish Chakraborty <150173837+ChromeFlaree@users.noreply.github.com>
2023-11-21 18:59:53 +0530

    Update Post.cpp
    Added new function that allows users to delete a post. Also changed like and comment function that checks if the post exists before adding like and comment.

--- Post.cpp
+++ Post.cpp
@@ -5,32 +5,45 @@ using namespace std;
 Post::Post(const shared_ptr<User> author, const string content) : author(author), content(content) {}
 
 void Post::addLike(const shared_ptr<User> user) {
-    if (user != author && author->isFriend(user) && !author->isBlocked(user)) {
+    if (deleted) {
+        cerr << "Error : Cannot like since post has been deleted." << endl;
+    } else if (user != author && author->isFriend(user) && !author->isBlocked(user)) {
         likes.insert(user);
         cout << user->getName() << " liked the post." << endl;
     } else {
-        cerr << "Error: " << user->getName() << " cannot like the post." << endl;
+        cerr << "Error : " << user->getName() << " cannot like the post." << endl;
     }
 }
 
 void Post::addComment(const shared_ptr<User> user, const string comment) {
-    if (user != author && author->isFriend(user) && !author->isBlocked(user)) {
+    if (deleted) {
+        cerr << "Error : Cannot comment since post has been deleted." << endl;
+    } else if (user != author && author->isFriend(user) && !author->isBlocked(user)) {
         comments.push_back({user, comment});
         cout << user->getName() << " commented on the post: " << comment << endl;
     } else {
-        cerr << "Error: " << user->getName() << " cannot comment on the post." << endl;
+        cerr << "Error : " << user->getName() << " cannot comment on the post." << endl;
     }
 }
 
+void Post::deletePost() {
+    deleted = true;
+    cout << author->getName() << " has deleted the post." << endl;
+}
+
 void Post::displayDetails() const {
-    cout << "Post by " << author->getName() << " : " << content << endl;
-    cout << "Likes : " << likes.size() << endl;
-    for (const auto user : likes) {
-        cout << user->getName() << " ";
-    }
-    cout << endl;
-    cout << "Comments : " << comments.size() << endl;
-    for (const auto comment : comments) {
-        cout << comment.first->getName() << " : " << comment.second << endl;
+    if (deleted) {
+        cout << "This post has been deleted." << endl;
+    } else {
+        cout << "Post by " << author->getName() << " : " << content << endl;
+        cout << "Likes : " << likes.size() << endl;
+        for (const auto user : likes) {
+            cout << user->getName() << " ";
+        }
+        cout << endl;
+        cout << "Comments : " << comments.size() << endl;
+        for (const auto comment : comments) {
+            cout << comment.first->getName() << " : " << comment.second << endl;
+        }
     }
 }
